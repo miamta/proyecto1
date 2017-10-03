34b779645595599689beb3ec9f7aa7f0b3bc0b64
Miguel Amores Tamarit <miamta@floridauniversitaria.es>
2017-10-03 13:10:43 +0200

    proyecto1

---
+++ proyecto.js
@@ -0,0 +1,11 @@
+// variablres
+var letra = "h";
+var letra2 = prompt('Ingrese una letra:','');
+
+
+// funcion if letra1
+if (letra==letra2){
+  alert("Acertaste")
+}else if (letra!=letra2) {
+    alert("FALLASTE")
+} ;
